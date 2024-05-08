e9316c916eb99f61415716dc31ba5008a47aba05
Siegfried Margin <siegfried.margin@gmail.com>
2024-05-08 12:38:30 +0200

    toggle function with [ngStyle]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  paragrafStatusOn: boolean = true;
+  serverStatus = true;
+  clicks = [];
 
-  paragrafOFF() {
-    this.paragrafStatusOn = !this.paragrafStatusOn;
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? false : true;
+  }
+
+  onCreateServer() {
+    console.log(this.serverStatus);
+    this.serverStatus = !this.serverStatus;
+    return this.serverStatus;
+
+    /*this.clicks.push(this.clicks.length + 1);
+    console.log(this.clicks); */
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
   }
 }
